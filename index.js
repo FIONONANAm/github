3a3c9c419481a69fc90ff152a93bff3580f0517d
FIONONANAm <rakfio20564@gmail.com>
2024-06-15 06:12:58 +0300

    Hello node

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Bonjour tous le monde !");
+// console.log("Bonjour tous le monde !
+import  express  from "express";
+
+const app = express();
+const port = 8000;
+
+app.get('/', (req, res)=>{
+  res.send('Bienvenue')
+})
+
+
+app.listen(port,()=>console.log(`le serveur a démarrer sur: http://localhost:${port}`));
